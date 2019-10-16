4e8da4f327ff35c4f5ac0f1d6d7e3a96f8739a19
Willian Lemann <willianlemann@hotmail.com>
2019-10-16 12:31:59 -0300

    final

--- js/index.js
+++ js/index.js
@@ -5,20 +5,47 @@ let img = document.getElementById('img');
 let icon = document.getElementById('icon');
 let degrees = document.getElementById('degrees');
 let items;
-let config = {
-    headers: {
-        'Authorization': '563492ad6f917000010000011c8584e295dd4fbe8fc94050b40b07e3'
-    }
-};
+
+
+
+
+
+
+
 
 function changeImages() {
 
-    axios.get('https://api.pexels.com/v1/curated?per_page=15&page=1', config) //https://api.unsplash.com/search/photos?query=landscape&client_id=2d888541fe80477a46b3c0888a8866a5c3fe7dcc3470346a633a2a6478990956
-        .then((res) => {
-            items = res.data.photos.url;
-        }).catch((err) => {
-            console.log(err);
+    axios.get('https://restcountries.eu/rest/v2/all')
+    .then((res) => {
+
+        let countriesArray = [];
+        res.data.map(item => {
+            countriesArray.push(item.name)
         });
+
+        axios.get(`https://api.unsplash.com/search/photos?query=${countriesArray[Math.round(Math.random() * 100)]}&client_id=2d888541fe80477a46b3c0888a8866a5c3fe7dcc3470346a633a2a6478990956`)
+            .then((res) => {
+                let array = [];
+                let data = res.data.results;
+                data.forEach(items => {
+                    array.push(items.urls.full)
+
+                    setInterval(() => {
+                        img.src = array[Math.round(Math.random() * 9)];
+                    }, 60000);
+
+                });
+
+
+            }).catch((err) => {
+                console.log(err);
+            });
+
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
 
     axios.get('https://api.openweathermap.org/data/2.5/weather?q=Porto%20Alegre&APPID=5eb7633b62139baf8138f658fed5f304')
         .then(function (res) {
@@ -29,10 +56,9 @@ function changeImages() {
         }).catch(function (error) {
             console.log('chegou aqui');
         })
-    setInterval(() => {
-        let randomNumber = Math.floor(Math.random() * 3);
 
-        img.src = `/assets/imagem${randomNumber}.jpg`;
+    setInterval(() => {
+
         icon.src = `http://openweathermap.org/img/wn/${iconWeather}@2x.png`;
         degrees.innerText = degreesWeather;
     }, 1000);
